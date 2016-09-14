7b6587731386eea0d90277f658cebc77e002739b
Leonardo Comelli <leonardo.comelli@gmail.com>
2016-09-14 01:51:22 +0000

    added nnp in the constructor

--- core/obfuscator.py
+++ core/obfuscator.py
@@ -3,12 +3,15 @@ import re
 class StringObfuscator:
 
 
-    def __init__(self, patterns = None):
+    def __init__(self, patterns = None, nnp = None):
       self.__patterns_registered = patterns
-
+      self.__nnp_registered = nnp
 
     def list_patterns(self):
         return self.__patterns_registered
+
+    def list_nnp(self):
+        return self.__nnp_registered
 
     def obfuscate(self, text):
         words = text.split(" ")
--- test/obfuscator_test.py
+++ test/obfuscator_test.py
@@ -6,11 +6,26 @@ class TestStringObfuscator(unittest.TestCase):
     def test_should_not_register_patterns(self):
         o = StringObfuscator()
         self.assertIsNone(o.list_patterns())
+        self.assertIsNone(o.list_nnp())
 
     def test_should_register_patterns(self):
         my_patterns = {'pattern1' : 'replacement1', 'pattern2' : 'replacement2'}
         o = StringObfuscator(my_patterns)
         self.assertEquals(len(o.list_patterns()), 2)
+        self.assertIsNone(o.list_nnp())
+
+    def test_should_register_nnp(self):
+        nnp = {'John' : '[NNP]', 'Steve' : '[NNP]'}
+        o = StringObfuscator(None, nnp)
+        self.assertEquals(len(o.list_nnp()), 2)
+        self.assertIsNone(o.list_patterns())
+
+    def test_should_register_both(self):
+        my_patterns = {'pattern1': 'replacement1', 'pattern2': 'replacement2'}
+        nnp = {'John' : '[NNP]', 'Steve' : '[NNP]'}
+        o = StringObfuscator(my_patterns, nnp)
+        self.assertEquals(len(o.list_patterns()), 2)
+        self.assertEquals(len(o.list_nnp()), 2)
 
     def test_should_replace_string(self):
         o = StringObfuscator({'secret' : '***'})
