45a7cdd1abd9216995af084b2e8c2ffa95b033c7
Leonardo Comelli <leonardo.comelli@gmail.com>
2016-09-14 02:42:10 +0000

    fixed a bug and DRY

--- core/obfuscator.py
+++ core/obfuscator.py
@@ -3,31 +3,31 @@ import re
 class StringObfuscator:
 
     DEFAULT_PATTERN_REPLACEMENT = '[PATTERN]'
+    DEFAULT_NNP_REPLACEMENT = '[NNP]'
 
     def __init__(self, patterns = None, nnp = None):
       self.__patterns_registered = patterns
       self.__nnp_registered = nnp
 
     def list_patterns(self):
-        return self.__patterns_registered
+        return self.__patterns_registered if self.__patterns_registered is not None else {}
 
     def list_nnp(self):
-        return self.__nnp_registered
+        return self.__nnp_registered if self.__nnp_registered is not None else {}
 
     def obfuscate(self, text):
         words = text.split(" ")
 
-        for i, w in enumerate(words):
-            nw = self.__replace_pattern__(w)
-            words[i] = nw if w != nw else w
+        self.__replace__(words, self.list_patterns(), self.DEFAULT_PATTERN_REPLACEMENT)
+        self.__replace__(words, self.list_nnp(), self.DEFAULT_NNP_REPLACEMENT)
 
         return " ".join(w for w in words)
 
+    def __replace__(self, words, patterns, default_replacement = None):
+        if len(patterns) == 0:
+            return words
 
-    def __replace_pattern__(self, word):
-        for p, r in self.__patterns_registered.items():
-          r = self.DEFAULT_PATTERN_REPLACEMENT if r == "" or r is None else r
-          return re.sub(p, r, word)
-
-        return word
-
+        for i, w in enumerate(words):
+            for p, r in patterns.items():
+                r = default_replacement if r == '' or r is None else r
+                words[i] = re.sub(p, r, w)
--- test/obfuscator_test.py
+++ test/obfuscator_test.py
@@ -5,51 +5,63 @@ class TestStringObfuscator(unittest.TestCase):
 
     def test_should_not_register_patterns(self):
         o = StringObfuscator()
-        self.assertIsNone(o.list_patterns())
-        self.assertIsNone(o.list_nnp())
+        self.assertEquals(0, len(o.list_patterns()))
+        self.assertEquals(0, len(o.list_nnp()))
 
     def test_should_register_patterns(self):
         my_patterns = {'pattern1' : 'replacement1', 'pattern2' : 'replacement2'}
         o = StringObfuscator(my_patterns)
-        self.assertEquals(len(o.list_patterns()), 2)
-        self.assertIsNone(o.list_nnp())
+        self.assertEquals(2, len(o.list_patterns()))
+        self.assertEquals(0, len(o.list_nnp()))
 
     def test_should_register_nnp(self):
         nnp = {'John' : '[NNP]', 'Steve' : '[NNP]'}
         o = StringObfuscator(None, nnp)
-        self.assertEquals(len(o.list_nnp()), 2)
-        self.assertIsNone(o.list_patterns())
+        self.assertEquals(0, len(o.list_patterns()))
+        self.assertEquals(2, len(o.list_nnp()))
 
     def test_should_register_both(self):
         my_patterns = {'pattern1': 'replacement1', 'pattern2': 'replacement2'}
         nnp = {'John' : '[NNP]', 'Steve' : '[NNP]'}
         o = StringObfuscator(my_patterns, nnp)
-        self.assertEquals(len(o.list_patterns()), 2)
-        self.assertEquals(len(o.list_nnp()), 2)
+        self.assertEquals(2, len(o.list_patterns()))
+        self.assertEquals(2, len(o.list_nnp()))
 
     def test_should_replace_string(self):
         o = StringObfuscator({'secret' : '***'})
         text = "his password is ***"
         obfuscated = o.obfuscate("his password is secret")
-        self.assertEqual(obfuscated, text)
+        self.assertEqual(text, obfuscated)
 
     def test_should_replace_string_with_basic_regex(self):
         o = StringObfuscator({'^s.*t$' : '***'})
         text = "his password is ***"
         obfuscated = o.obfuscate("his password is secret")
-        self.assertEqual(obfuscated, text)
+        self.assertEqual(text, obfuscated)
 
     def test_should_replace_string_with_complex_regex(self):
         o = StringObfuscator({'[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}': '[CPF]'})
         text = "my cpf number is [CPF]"
         obfuscated = o.obfuscate("my cpf number is 564.987.456-99")
-        self.assertEqual(obfuscated, text)
+        self.assertEqual(text, obfuscated)
+
+    def test_should_replace_two_strings_with_basic_regex(self):
+        o = StringObfuscator({'^s.*t$' : '***'})
+        text = "ah *** -> his password is ***"
+        obfuscated = o.obfuscate("ah secret -> his password is secret")
+        self.assertEqual(text, obfuscated)
 
     def test_should_replace_string_default_replacement(self):
         o = StringObfuscator({'^s.*t$': ''})
         text = "his password is [PATTERN]"
         obfuscated = o.obfuscate("his password is secret")
-        self.assertEqual(obfuscated, text)
+        self.assertEqual(text, obfuscated)
+
+    def test_should_replace_nnp(self):
+        o = StringObfuscator(None, {'John' : '', 'Doe' : ''})
+        text = "his name is [NNP] [NNP]"
+        obfuscated = o.obfuscate("his name is John Doe")
+        self.assertEqual(text, obfuscated)
 
 if __name__ == '__main__':
     unittest.main()
